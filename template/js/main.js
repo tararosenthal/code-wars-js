25cbd611cd67ebca1796c9b1007f47e7976eee2b
tararosenthal <65468485+tararosenthal@users.noreply.github.com>
2022-03-26 16:11:15 -0500

    Pagination class

--- template/js/main.js
+++ template/js/main.js
@@ -40,3 +40,62 @@ function add(n){
 
   return fn;
 }
+
+// TODO: complete this object/class
+let collection;
+let itemsPerPage;
+let pages;
+
+// The constructor takes in an array of items and a integer indicating how many
+// items fit within a single page
+function PaginationHelper(collection, itemsPerPage){
+  this.collection = collection;
+  this.itemsPerPage = itemsPerPage;
+
+  this.pages = collection.reduce((array, item, index) => {
+    let currentPage = index / itemsPerPage | 0;
+    if (array[currentPage] === undefined) {
+      array.push([item]);
+    } else {
+      array[currentPage].push(item);
+    }
+    return array;
+  }, []);
+}
+
+// returns the number of items within the entire collection
+PaginationHelper.prototype.itemCount = function() {
+  return this.collection.length;
+}
+
+// returns the number of pages
+PaginationHelper.prototype.pageCount = function() {
+  return this.pages.length;
+}
+
+// returns the number of items on the current page. page_index is zero based.
+// this method should return -1 for pageIndex values that are out of range
+PaginationHelper.prototype.pageItemCount = function(pageIndex) {
+  if (pageIndex > this.pages.length - 1 || pageIndex < 0) {
+    return -1;
+  } else {
+    return this.pages[pageIndex].length;
+  }
+}
+
+// determines what page an item is on. Zero based indexes
+// this method should return -1 for itemIndex values that are out of range
+PaginationHelper.prototype.pageIndex = function(itemIndex) {
+  if (itemIndex > this.collection.length - 1 || itemIndex < 0) {
+    return -1;
+  } else {
+    return itemIndex / this.itemsPerPage | 0;
+  }
+}
+
+let example = new PaginationHelper(['a','b','c','d','e','f'], 4);
+
+console.log("item count: " + example.itemCount());
+console.log("page count: " + example.pageCount());
+console.log("pageItemCount: " + example.pageItemCount(1));
+console.log("page index: " + example.pageIndex(5));
